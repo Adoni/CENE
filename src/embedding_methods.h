cdd559bdbc0b9be476872f6837337ff51e3a20a2
Sun Xiaofei <adoni1203@gmail.com>
2016-08-27 11:41:09 +0800

    add pretraining

--- src/embedding_methods.h
+++ src/embedding_methods.h
@@ -11,6 +11,7 @@
 #include "cnn/lstm.h"
 #include "cnn/gru.h"
 #include "cnn/dict.h"
+#include "cnn/training.h"
 #include <vector>
 #include <string>
 
@@ -21,6 +22,8 @@ public:
     virtual ~ContentEmbeddingMethod() { }
 
     virtual Expression get_embedding(const CONTENT_TYPE &content, const TFIDF_TYPE &tfidf, ComputationGraph &cg) = 0;
+
+    virtual void pretraining(GraphData &graph_data,Model &model)=0;
 
     std::string get_method_name() {
         return method_name;
@@ -107,11 +110,14 @@ public:
         }
         return average(all_word_embedding);
     }
+    void pretraining(GraphData &graph_data,Model &model){}
 };
 
 class GRU_CE : public ContentEmbeddingMethod {
 public:
     GRUBuilder builder;
+    Parameters* p_R;
+    Parameters* p_bias;
 
     explicit GRU_CE(
             Model &model,
@@ -123,8 +129,11 @@ public:
         this->C_EM_DIM = content_embedding_size;
         this->method_name = "GRU";
         this->use_const_lookup=use_const_lookup;
+
         builder = GRUBuilder(1, W_EM_DIM, C_EM_DIM, &model);
         p = model.add_lookup_parameters(d.size(), {W_EM_DIM});
+        p_R = model.add_parameters({d.size(), content_embedding_size});
+        p_bias = model.add_parameters({d.size()});
         initial_look_up_table(d.size());
     }
 
@@ -144,6 +153,43 @@ public:
             }
         }
         return average(all_hidden);
+    }
+
+    void pretraining(GraphData &graph_data, Model &model){
+        Trainer* sgd = nullptr;
+        sgd = new SimpleSGDTrainer(&model);
+        int counter=0;
+        int total=graph_data.id_map.id_to_content.size();
+        float loss=0.0;
+        for(auto& content:graph_data.id_map.id_to_content){
+            std::cout<<"Pretraining: "<<counter<<"/"<<total<<std::endl;
+            for(auto& sent:content){
+                ComputationGraph cg;
+                sentence_pretraining(sent,cg);
+                loss += as_scalar(cg.forward());
+                cg.backward();
+                sgd->update();
+            }
+            counter+=1;
+        }
+    }
+
+    Expression sentence_pretraining(const SENT_TYPE  &sent, ComputationGraph& cg){
+        const unsigned slen = sent.size();
+        builder.new_graph(cg);  // reset RNN builder for new graph
+        builder.start_new_sequence();
+        Expression i_R = parameter(cg, p_R); // hidden -> word rep parameter
+        Expression i_bias = parameter(cg, p_bias);  // word bias
+        std::vector<Expression> errs;
+        for (unsigned t = 0; t < slen-1; ++t) {
+            Expression i_x_t = lookup(cg, p, sent[t]);
+            Expression i_y_t = builder.add_input(i_x_t);
+            Expression i_r_t =  i_bias + i_R * i_y_t;
+            Expression i_err = pickneglogsoftmax(i_r_t, sent[t+1]);
+            errs.push_back(i_err);
+        }
+        Expression i_nerr = sum(errs);
+        return i_nerr;
     }
 };
 
@@ -204,6 +250,8 @@ public:
         }
         return cnn::expr::parameter(cg, p_W) * cnn::expr::concatenate(tmp);
     }
+    void pretraining(GraphData &graph_data,Model &model){}
+
 
     std::vector<std::vector<cnn::Parameters*>> p_filters;
     std::vector<std::vector<cnn::Parameters*>> p_biases;
